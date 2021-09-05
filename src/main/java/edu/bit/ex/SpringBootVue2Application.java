8713ec4f849be08e1d572abcf58690b2aa302385
nqwrt <nqwrt@ymail.com>
2021-09-05 17:09:53 +0900

    first commit

---
+++ src/main/java/edu/bit/ex/SpringBootVue2Application.java
@@ -0,0 +1,13 @@
+package edu.bit.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVue2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVue2Application.class, args);
+	}
+
+}
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="안녕하세요 Vue.js 앱입니다." />
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  
+  export default {
+    name: 'home',
+    components: {
+      HelloWorld
+    }
+  };
+</script>
